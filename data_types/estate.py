62562b558373ef7ed68d7821946b763abe5cea14
tom <t-gutowski@o2.pl>
2023-03-18 18:05:25 +0100

    refactor: remove unused import and variable

--- data_types/estate.py
+++ data_types/estate.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel
 
-from config.config_handler import ParametersHandler
 from data_types.estate_details import EstateDetails
-
-PARAMS = ParametersHandler().get_params()
 
 
 class Estate(BaseModel):
