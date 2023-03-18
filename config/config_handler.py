e3a2ba03ce0aea2f78bde88cf4253bcea343c6d1
tom <t-gutowski@o2.pl>
2023-03-18 13:29:00 +0100

    feat: add config files

---
+++ config/config_handler.py
@@ -0,0 +1,27 @@
+from os import path
+from typing import Any, Dict
+
+import yaml
+
+
+class ParametersHandler:
+    """
+    Helper function to extract params from yaml and return predefined params.
+    """
+    def __init__(self) -> None:
+        self.handler: Dict[Any, Any]
+        current_directory: str = path.dirname(path.realpath(__file__))
+        file_path: str = path.normpath(
+            path.join(current_directory, "parameters.yaml")
+        )
+        with open(file_path, "r", encoding="utf-8") as f:
+            self.handler = yaml.safe_load(f)
+
+        if not self.handler:
+            raise ValueError("Parameters file is empty or not exists.")
+
+    def get_params(self) -> Dict[str, Any]:
+        """
+        Return all handler params from "parameters.yaml" in a Dict.
+        """
+        return self.handler if self.handler else {}
