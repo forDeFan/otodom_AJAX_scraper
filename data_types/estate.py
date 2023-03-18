6135da325188e0ad0f0cb0c175f97618e8e1b460
tom <t-gutowski@o2.pl>
2023-03-18 13:29:48 +0100

    feat: add Estate model

---
+++ data_types/estate.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+from config.config_handler import ParametersHandler
+from data_types.estate_details import EstateDetails
+
+PARAMS = ParametersHandler().get_params()
+
+
+class Estate(BaseModel):
+    """
+    Estate model class.
+    """
+
+    url: str
+    details: EstateDetails
+
+    class Config:
+        allow_extra = False
