6795c8da51b26117c0d8d07a6af9b415c87b2f5c
tom <t-gutowski@o2.pl>
2023-03-22 20:13:03 +0100

    test: add test for Estate model

---
+++ tests/test_estate.py
@@ -0,0 +1,62 @@
+import pytest
+from pydantic.error_wrappers import ValidationError
+
+from data_types.estate import Estate
+
+
+class TestEstate:
+    def test_if_model_created(self):
+        expected = {
+            "url": "test_url",
+            "details": {
+                "price": "test_price",
+                "size": "test_size",
+                "location": "test_location",
+                "description": "test_description",
+            },
+        }
+
+        model = Estate(
+            url="test_url",
+            details={
+                "price": "test_price",
+                "size": "test_size",
+                "location": "test_location",
+                "description": "test_description",
+            },
+        )
+
+        assert model.dict() == expected
+        assert isinstance(model, Estate)
+
+    def test_if_exception_when_no_url(self):
+        with pytest.raises(ValidationError):
+            Estate(
+                url=None,
+                details={
+                    "price": "test_price",
+                    "size": "test_size",
+                    "location": "test_location",
+                    "description": "test_description",
+                },
+            )
+
+    def test_if_exception_when_no_details(self):
+        with pytest.raises(ValidationError):
+            Estate(url="test_url", details=None)
+
+    def test_if_exception_when_details_not_dict(self):
+        with pytest.raises(ValidationError):
+            Estate(url="test_url", details="no_dict_data_type")
+
+    def test_if_exception_when_wrong_details(self):
+        with pytest.raises(ValidationError):
+            Estate(
+                url="test_url",
+                details={
+                    "wrong_field": "test_price",
+                    "size": "test_size",
+                    "location": "test_location",
+                    "description": "test_description",
+                },
+            )
